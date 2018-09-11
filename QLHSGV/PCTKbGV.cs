96ba762636d113d30a0c14d532fd90a71f06a0a2
winsbu1997 <winsbu1997@gmail.com>
2018-09-11 09:17:31 +0700

    Lan 1

---
+++ QLHSGV/PCTKbGV.cs
@@ -0,0 +1,23 @@
+﻿using QLHSGV._GV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHSGV
+{
+    public partial class PCTKbGV : Form
+    {
+        public PCTKbGV()
+        {
+            InitializeComponent();
+            panel1.Controls.Add(usr_GV.Instance);
+            usr_GV.Instance.Dock = DockStyle.Fill;
+        }
+    }
+}
