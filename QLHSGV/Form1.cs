745a326384992af91b2171ae9122be5af53677a6
luan127 <127.trancongluan@gmail.com>
2018-11-15 20:14:00 +0700

    Luan 1

---
+++ QLHSGV/Form1.cs
@@ -0,0 +1,23 @@
+﻿using QLHSGV._HS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHSGV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Controls.Add(usr_HS.Instance);
+            usr_HS.Instance.Dock = DockStyle.Fill;
+        }
+    }
+}
