ccce7bad0f921b788bfa3c4864f7d77f055d26ff
viet1997 <37035291+viet1997@users.noreply.github.com>
2018-11-16 15:58:12 +0700

    viet lan 3

---
+++ QLHSGV/GiaoVienDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLHSGV.EF;
+
+namespace QLHSGV.DAO
+{   
+    public  class GiaoVienDAO
+    {
+        HSGVContext db = new HSGVContext();
+
+        public List<GiaoVien> ListAll()
+        {
+            return db.GiaoViens.ToList();
+        }
+
+
+        public bool Delete(string MaHS)
+        {
+            var dbEntry = db.GiaoViens.Find(MaHS);
+            if(dbEntry == null)
+            {
+                return false;
+            }
+            else
+            {
+                db.GiaoViens.Remove(dbEntry);
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+
+    }
+}
